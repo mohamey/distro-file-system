0d1dc2a419e0bedae36b2cdd5b89d33e546aed50
Yasir Mohamed <mohamey@tcd.ie>
2016-11-29 11:17:42 +0000

    Separated FileServer module from main

--- app/Main.hs
+++ app/Main.hs
@@ -1,71 +1,7 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE TypeFamilies #-}
-
 module Main where
 
-
 import Lib
-
-import Prelude ()
-import Prelude.Compat
-
-import Control.Monad.Except
-import Control.Monad.Reader
-import Data.Aeson.Compat
-import Data.Aeson.Types
-import Data.Attoparsec.ByteString
-import Data.ByteString (ByteString)
-import Data.List
-import Data.Maybe
-import Data.String.Conversions
-import Data.Time.Calendar
-import GHC.Generics
-import Lucid
-import Network.HTTP.Media ((//), (/:))
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Servant
-import System.Directory
-import Text.Blaze
-import Text.Blaze.Html.Renderer.Utf8
-import qualified Data.Aeson.Parser
-import qualified Text.Blaze.Html
-
-data User = User
-  {
-    name :: String
-    , age :: Int
-    , email :: String
-    , registration_date :: Day 
-  } deriving (Eq, Show, Generic)
-
-instance ToJSON User
-
-users1 :: [User]
-users1 =
-  [
-    User "Isaac Newton" 371 "isaac@newton.co.uk" (fromGregorian 1683 3 1)
-  , User "Albert Einstein" 136 "ae@mc2.org" (fromGregorian 1905 12 1)
-  ]
-
-type UserAPI1 = "users" :> Get '[JSON] [User]
--- type UserAPI1 = "users" :> Get [JSON] [User]
-
-server1 :: Server UserAPI1
-server1 = return users1
-
-userAPI :: Proxy UserAPI1
-userAPI = Proxy
-
-app1 :: Application
-app1 = serve userAPI server1
+import FileServer
 
 main :: IO ()
-main = run 8080 app1
+main = runServer 8080
