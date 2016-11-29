51c91d7ad087a5ba73997c497797fcd37c4d22ef
Yasir Mohamed <mohamey@tcd.ie>
2016-11-29 18:52:23 +0000

    Single file server responds to get requests

--- app/FileServer.hs
+++ app/FileServer.hs
@@ -14,76 +14,38 @@ module FileServer
 import Prelude ()
 import Prelude.Compat
 
-import Control.Monad.Except
-import Control.Monad.Reader
-import Data.Aeson.Compat
-import Data.Aeson.Types
-import Data.Attoparsec.ByteString
-import qualified Data.ByteString as B
-import Data.List
-import Data.Maybe
-import Data.String.Conversions
-import Data.Time.Calendar
-import GHC.Generics
-import Lucid
-import Network.HTTP.Media ((//), (/:))
-import Network.Wai
+-- import Control.Monad.Except
+-- import Control.Monad.Reader
+-- import Data.Aeson.Compat
+-- import Data.Aeson.Types
+-- import Data.Attoparsec.ByteString
+-- import Data.ByteString
+-- import Data.List
+-- import Data.Maybe
+-- import Data.String.Conversions
+-- import Data.Time.Calendar
+-- import GHC.Generics
+-- import Lucid
+-- import Network.HTTP.Media ((//), (/:))
+-- import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
-import Servant.Utils.StaticFiles
-import System.Directory
-import Text.Blaze
-import Text.Blaze.Html.Renderer.Utf8
-import qualified Data.Aeson.Parser
-import qualified Text.Blaze.Html
+-- import System.Directory
+-- import Text.Blaze
+-- import Text.Blaze.Html.Renderer.Utf8
+-- import qualified Data.Aeson.Parser
+-- import qualified Text.Blaze.Html
 
-data User = User
-  {
-    name :: String
-    , age :: Int
-    , email :: String
-    , registration_date :: Day 
-  } deriving (Eq, Show, Generic)
+type FileAPI = "files" :> Raw
 
-instance ToJSON User
+fileAPI :: Proxy FileAPI
+fileAPI = Proxy
 
-users1 :: [User]
-users1 =
-  [
-    User "Isaac Newton" 371 "isaac@newton.co.uk" (fromGregorian 1683 3 1)
-  , User "Albert Einstein" 136 "ae@mc2.org" (fromGregorian 1905 12 1)
-  ]
-
-type UserAPI1 = "users" :> Get '[JSON] [User]
-
-
-server1 :: Server UserAPI1
-server1 = return users1
-
-userAPI :: Proxy UserAPI1
-userAPI = Proxy
+server1 :: Server FileAPI
+server1 = serveDirectory "static-files"
 
 app1 :: Application
-app1 = serve userAPI server1
-
--- data ServerFile = ServerFile
---   {
---     fileName :: String
---     , file :: B.ByteString
---   } deriving (Eq, Show, Generic)
-
--- instance ToJSON ServerFile
-
-newtype BS = BS [B.ByteString]
-  deriving Generic
-
-type FileAPI = "file" :> Raw
-
--- fileAPI :: Proxy FileAPI
--- fileAPI = Proxy
-
-server2 :: Server FileAPI
-server2 = serveDirectory "file"
+app1 = serve fileAPI server1
 
 runServer :: Int -> IO ()
-runServer port = run port server2
+runServer port = run port app1
