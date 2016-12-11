2d9f5307f922b6c156234be27dd20f9eef7cfab0
Yasir Mohamed <mohamey@tcd.ie>
2016-12-11 13:51:34 +0000

    Working post requests from the client

--- Client/app/Client.hs
+++ Client/app/Client.hs
@@ -6,10 +6,13 @@ module Client where
 
 import Lib
 
+import Control.Monad.IO.Class
 import Data.Proxy as DP
+import qualified Data.Text.Lazy.IO as TLIO
 import Network.HTTP.Client
 import Servant.API
 import Servant.Client
+import System.Directory
 
 upload :: FileObject -> ClientM ApiResponse
 remove :: ObjIdentifier -> ClientM ApiResponse
@@ -27,6 +30,11 @@ getRequest reqPath = do
   fi <- get (Just reqPath)
   return fi
 
+postRequest :: FileObject -> ClientM ApiResponse
+postRequest postFile = do
+  res <- upload postFile
+  return res
+
 -- Args list should just have one index, the requested path
 parseCommand :: String -> [String] -> IO ()
 parseCommand _ [] = putStrLn "No Arguments provided"
@@ -37,6 +45,22 @@ parseCommand "get" (p:_) = do
     Left err -> putStrLn $ "Error: " ++ show err
     Right fi -> do
       putStrLn $ show (fileContent fi)
+parseCommand "post" (f:_) = liftIO $ do
+  manager <- newManager defaultManagerSettings
+  doesFileExist f >>=
+    (\res -> if res then
+        TLIO.readFile f >>=
+          (\fileText -> runClientM (postRequest (FileObject f fileText)) (ClientEnv manager url) >>=
+            (\response -> case response of
+                Left err -> putStrLn $ "Error: " ++ show err
+                Right apiRes -> do
+                  case (result apiRes) of
+                    False -> do
+                      putStrLn $ "Post failed: " ++ (message apiRes)
+                    True -> do
+                      putStrLn $ "Post Successful: " ++ message(apiRes)))
+   else do
+        putStrLn "File not found")
 
 parseCommand _ _ = putStrLn "Command unrecognized"
 
