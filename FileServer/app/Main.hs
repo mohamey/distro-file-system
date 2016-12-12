e9fb4468bf8302ac156509c13e98a63c0284585a
Yasir Mohamed <mohamey@tcd.ie>
2016-12-12 14:47:38 +0000

    Fileserver takes port as command line arg

--- FileServer/app/Main.hs
+++ FileServer/app/Main.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
 import FileServer
+import System.Environment
 
 main :: IO ()
-main = runServer 8080
+main = do --runServer port
+  getArgs >>=
+    (\args -> let port = read (head args) :: Int in runServer port)
